2d01c4e8abbbde528141ac68b1bc6f7214761f50
Jermolene <jeremy@osmosoft.com>
2014-01-16 16:48:45 +0000

    Run TiddlyWikis in a sandbox
    Now we’re using the special sandbox iframe feature of node-webkit to
    embed the TiddlyWiki document, and using DOM events and attributes for
    communication with the TiddlyDesktop host.

--- source/js/main.js
+++ source/js/main.js
@@ -16,7 +16,7 @@ var wikiList = [];
 
 // Get the main window
 var mainWindow = gui.Window.get();
-mainWindow.showDevTools();
+// mainWindow.showDevTools();
 
 // Current window
 var currentWindow = null;
@@ -25,7 +25,7 @@ var currentWindow = null;
 var shuttingDown = false;
 
 // Get the current wikiList
-//loadWikiList();
+loadWikiList();
 
 // Close all windows when the current window is closed
 mainWindow.on("close",function() {
@@ -38,7 +38,7 @@ mainWindow.on("close",function() {
 trackCurrentWindow(mainWindow);
 
 // Add a menubar
-//addMenuBar(mainWindow);
+addMenuBar(mainWindow);
 
 // Trap clicks on wikilinks
 trapLinks(document);
@@ -58,12 +58,21 @@ mainWindow.on("loaded",function() {
 // Event handlers for browsing for a new wiki
 var chooseWiki = document.getElementById("chooseWiki");
 chooseWiki.addEventListener("change",function(event) {
-	openWikiIfNotOpen("file://" + chooseWiki.value);
+	openWikiIfNotOpen(convertPathToFileUrl(chooseWiki.value));
 },false);
 var btnChooseWiki = document.getElementById("btnChooseWiki");
 btnChooseWiki.addEventListener("click",function(event) {
 	chooseWiki.click();
 },false);
+
+function convertPathToFileUrl(path) {
+	// File prefix depends on platform
+	var fileUriPrefix = "file://";
+	if(process.platform.substr(0,3) === "win") {
+		fileUriPrefix = fileUriPrefix + "/";
+	}
+	return fileUriPrefix + path.replace(/\\/g,"/");
+}
 
 function openWikiIfNotOpen(wikiUrl) {
 	var wikiInfo = findwikiInfo(wikiUrl);
@@ -75,19 +84,20 @@ function openWikiIfNotOpen(wikiUrl) {
 
 // Helper to open a TiddlyWiki in a new window
 function openWiki(wikiUrl) {
+console.log("Opening wiki",wikiUrl)
 	// Add the path to the wikiList if not already there
 	var wikiInfo = findwikiInfo(wikiUrl);
 	if(wikiInfo === null) {
 		wikiInfo = {url: wikiUrl};
+		wikiList[wikiList.length] = wikiInfo;
 	}
 	// Save the wiki list and update it in the DOM
-	wikiList[wikiList.length] = wikiInfo;
 	wikiInfo.isOpen = true;
 	saveWikiList();
 	renderWikiList();
 	// Open the window
-	var newWindow = gui.Window.open(wikiUrl,{
-		toolbar: true,
+	var newWindow = gui.Window.open("./host.html",{
+		toolbar: false,
 		focus: true,
 		width: 1024,
 		height: 768,
@@ -104,16 +114,67 @@ function openWiki(wikiUrl) {
 	});
 	// Set up the new window when loaded
 	newWindow.on("loaded",function() {
-		newWindow.showDevTools();
+console.log("newWindow is loaded")
+		// if(process.platform !== "darwin") {
+		// 	addMenuBar(newWindow);
+		// }
 		trackCurrentWindow(newWindow);
-		trapLinks(newWindow.window.document);
-		newWindow.capturePage(function(imgDataUri) {
-			wikiInfo.img = imgDataUri;
-			wikiInfo.title = newWindow.title;
-			saveWikiList();
-			renderWikiList();
-		},"png");
+		// newWindow.showDevTools();
+		var hostIframe = newWindow.window.document.getElementById("twFrame");
+console.log(hostIframe.src,wikiUrl)
+		if(hostIframe.src !== wikiUrl) {
+			hostIframe.addEventListener("load",function(event) {
+console.log("hostIframe is loaded")
+				enableSaving(hostIframe.contentWindow,wikiUrl);
+				trapLinks(hostIframe.contentWindow.document);
+				newWindow.capturePage(function(imgDataUri) {
+console.log("window is captured")
+					wikiInfo.img = imgDataUri;
+					var title = hostIframe.contentWindow.document.title;
+					newWindow.window.document.title = title;
+					wikiInfo.title = title;
+					saveWikiList();
+					renderWikiList();
+				},"png");
+				event.stopPropagation();
+				event.preventDefault();
+				return false;
+			},false);
+			hostIframe.src = wikiUrl;
+		}
 	});
+}
+
+// Helper to enable TiddlyFox-style saving for a window
+function enableSaving(win,wikiUrl) {
+	// Create the message box
+	var doc = win.document,
+		messageBox = doc.createElement("div");
+	messageBox.id = "tiddlyfox-message-box";
+	doc.body.appendChild(messageBox);
+	// Listen for save events
+	messageBox.addEventListener("tiddlyfox-save-file",function(event) {
+		// Get the details from the message
+		var message = event.target,
+			path = message.getAttribute("data-tiddlyfox-path"),
+			content = message.getAttribute("data-tiddlyfox-content");
+		// Save the file
+		saveFile(path,content);
+		// Remove the message element from the message box
+		message.parentNode.removeChild(message);
+		// Send a confirmation message
+		var event = doc.createEvent("Events");
+		event.initEvent("tiddlyfox-have-saved-file",true,false);
+		event.savedFilePath = path;
+		message.dispatchEvent(event);
+		return false;
+	},false);
+}
+
+// Helper function to save a file
+function saveFile(path,content) {
+	var fs = require("fs");
+	fs.writeFileSync(path,content);
 }
 
 // Helper to find an entry in the wiki list
@@ -138,26 +199,26 @@ function trackCurrentWindow(win) {
 // Helper to add a menubar to a window
 function addMenuBar(win) {
 	win.menu = new gui.Menu({ type: "menubar" });
-	var fileMenu = new gui.MenuItem({
-	    label: "File",
-	    submenu: new gui.Menu()
-	});
-	win.menu.insert(fileMenu,1);
-	fileMenu.submenu.append(new gui.MenuItem({
-	    label: "New",
-	    click: function () {
-	    	alert("New!!!!")
-	    }
-	}));
-	fileMenu.submenu.append(new gui.MenuItem({
-	    type: "separator"
-	}));
-	fileMenu.submenu.append(new gui.MenuItem({
-	    label: "Close",
-	    click: function () {
-	        win.close();
-	    }
-	}));
+	// var fileMenu = new gui.MenuItem({
+	//     label: "File",
+	//     submenu: new gui.Menu()
+	// });
+	// win.menu.insert(fileMenu,1);
+	// fileMenu.submenu.append(new gui.MenuItem({
+	//     label: "New",
+	//     click: function () {
+	//     	alert("New!!!!")
+	//     }
+	// }));
+	// fileMenu.submenu.append(new gui.MenuItem({
+	//     type: "separator"
+	// }));
+	// fileMenu.submenu.append(new gui.MenuItem({
+	//     label: "Close",
+	//     click: function () {
+	//         win.close();
+	//     }
+	// }));
 	var developerMenu = new gui.MenuItem({
 	    label: "Developer",
 	    submenu: new gui.Menu()
