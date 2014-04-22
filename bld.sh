ede5784dee3d63192e9c15565a8d72e52ceab8fe
Jermolene <jeremy@osmosoft.com>
2014-04-22 10:41:45 +0100

    Update to node-webkit 0.9.2

--- bld.sh
+++ bld.sh
@@ -13,7 +13,7 @@ mkdir -p output/linux32
 mkdir -p output/linux64
 
 # Copy the OS X App
-cp -R node-webkit/node-webkit-v0.8.4-osx-ia32/node-webkit.app output/mac/TiddlyWiki.app
+cp -R node-webkit/node-webkit-v0.9.2-osx-ia32/node-webkit.app output/mac/TiddlyWiki.app
 
 # Copy the OS X app folder
 cp -R source output/mac/TiddlyWiki.app/Contents/Resources/app.nw
@@ -25,19 +25,19 @@ cp icons/app.icns output/mac/TiddlyWiki.app/Contents/Resources/nw.icns
 cp Info.plist output/mac/TiddlyWiki.app/Contents/Info.plist
 
 # Copy the Windows App
-cp -R node-webkit/node-webkit-v0.8.4-win-ia32/* output/win
+cp -R node-webkit/node-webkit-v0.9.2-win-ia32/* output/win
 
 # Copy our source into it
 cp -R source/* output/win
 
 # Copy the Linux32 App
-cp -R node-webkit/node-webkit-v0.8.4-linux-ia32/* output/linux32
+cp -R node-webkit/node-webkit-v0.9.2-linux-ia32/* output/linux32
 
 # Copy our source into Linux32
 cp -R source/* output/linux32
 
 # Copy the Linux64 App
-cp -R node-webkit/node-webkit-v0.8.4-linux-x64/* output/linux64
+cp -R node-webkit/node-webkit-v0.9.2-linux-x64/* output/linux64
 
 # Copy our source into Linux64
 cp -R source/* output/linux64
