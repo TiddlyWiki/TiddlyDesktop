f907c83e3816118fc6160e328fefff459d9db86e
Jermolene <jeremy@osmosoft.com>
2014-12-21 11:44:39 +0000

    Clarify message names

--- source/base-config-tiddlywiki/tiddlers/modules/startup/handlers.js
+++ source/base-config-tiddlywiki/tiddlers/modules/startup/handlers.js
@@ -18,12 +18,12 @@ exports.after = ["startup"];
 exports.synchronous = true;
 
 exports.startup = function() {
-	$tw.rootWidget.addEventListener("tiddlydesktop-open-wiki",function(event) {
+	$tw.rootWidget.addEventListener("tiddlydesktop-open-wiki-url",function(event) {
 		// Open the TiddlyWiki window
 		$tw.desktop.configWindow.openHostWindowByUrl(event.param);
 		return false;
 	});
-	$tw.rootWidget.addEventListener("tiddlydesktop-open-wiki-files",function(event) {
+	$tw.rootWidget.addEventListener("tiddlydesktop-open-wiki-path",function(event) {
 		for(var t=0; t<event.param.length; t++) {
 			var file = event.param[t];
 			$tw.desktop.configWindow.openHostWindowByPath(file.path);
