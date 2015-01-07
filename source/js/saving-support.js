d7a69b04ff432af9cfcb151490de311d1d5d6e2b
Jermolene <jeremy@osmosoft.com>
2015-01-07 17:26:07 +0000

    Add support for backups

--- source/js/saving-support.js
+++ source/js/saving-support.js
@@ -17,16 +17,18 @@ exports.enableSaving = function(doc) {
 	messageBox.addEventListener("tiddlyfox-save-file",function(event) {
 		// Get the details from the message
 		var message = event.target,
-			path = message.getAttribute("data-tiddlyfox-path"),
+			filepath = message.getAttribute("data-tiddlyfox-path"),
 			content = message.getAttribute("data-tiddlyfox-content");
+		// Backup the existing file (if any)
+		backupFile(filepath);
 		// Save the file
-		saveFile(path,content);
+		saveFile(filepath,content);
 		// Remove the message element from the message box
 		message.parentNode.removeChild(message);
 		// Send a confirmation message
 		var event = doc.createEvent("Events");
 		event.initEvent("tiddlyfox-have-saved-file",true,false);
-		event.savedFilePath = path;
+		event.savedFilePath = filepath;
 		message.dispatchEvent(event);
 		return false;
 	},false);
@@ -53,9 +55,43 @@ function injectClassicOverrides(doc) {
 
 
 // Helper function to save a file
-function saveFile(path,content) {
+function saveFile(filepath,content) {
 	var fs = require("fs");
-	fs.writeFileSync(path,content);
+	fs.writeFileSync(filepath,content);
+}
+
+// Helper function to backup a file by copying it to the backup folder
+function backupFile(filepath) {
+	var fs = require("fs"),
+		path = require("path");
+	// Backup the file if it exists
+	if(fs.existsSync(filepath)) {
+		// Get the timestamp
+		var timestamp = $tw.utils.stringifyDate(fs.statSync(filepath).mtime || (new Date())),
+			backupSubPath = $tw.wiki.getTiddlerText("$:/TiddlyDesktop/BackupPath",""),
+			filename = path.basename(filepath);
+		// Replace $filename$ with the filename portion of the filepath and $filepath$ with the entire filepath 
+		backupSubPath = backupSubPath
+			.replace(/\$filename\$/mgi,filename)
+			.replace(/\$filepath\$/mgi,filepath);
+		// Compose and uniquify the backup pathname
+		var count = 0,
+			backupPath,
+			uniquifier;
+		do {
+			uniquifier = count ? " " + count : "";
+			backupPath = path.resolve(
+				path.dirname(filepath),
+				backupSubPath,
+				filename + "." + timestamp + uniquifier + path.extname(filepath)
+			);
+			count = count + 1;
+		} while(fs.existsSync(backupPath));
+		// Copy the existing file to the backup
+console.log("Backing up",filepath,backupPath);
+		$tw.utils.createDirectory(path.dirname(backupPath));
+		fs.writeFileSync(backupPath,fs.readFileSync(filepath)); // For some reason $tw.utils.copyFile() doesn't work here
+	}
 }
 
 })();
