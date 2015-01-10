7360a38301492ffce0dc46d9ccb0396ad45a9f83
Jermolene <jeremy@osmosoft.com>
2015-01-10 18:47:24 +0000

    Fix external links in tiddler windows

--- source/js/tiddler-windows.js
+++ source/js/tiddler-windows.js
@@ -84,6 +84,8 @@ function TiddlerWindow(options,tiddlerWindowIdentifier) {
 		// Trap developer tools on F12
 		devTools.trapDevTools(self.window,self.window.window.document);
 // self.window.showDevTools();
+		// Trap external links
+		$tw.desktop.trapLinks(doc);
 		// Make $tw available in the window
 		self.window.window.$tw = $tw;
 		// Set up the title
