40bd530e9fd3fb9cae3350a77fe39a11ce4b06cd
Jermolene <jeremy@jermolene.com>
2015-10-24 14:30:50 +0100

    Add distribution directory to contain TiddlyDesktop files
    Fixes #37

--- bld.sh
+++ bld.sh
@@ -21,36 +21,42 @@ cp -R node_modules/tiddlywiki source/tiddlywiki
 # Create the output directory
 mkdir -p output
 mkdir -p output/mac32
+mkdir -p output/mac32/TiddlyDesktop-mac32-v0.0.5
 mkdir -p output/mac64
+mkdir -p output/mac64/TiddlyDesktop-mac64-v0.0.5
 mkdir -p output/win32
+mkdir -p output/win32/TiddlyDesktop-win32-v0.0.5
 mkdir -p output/win64
+mkdir -p output/win64/TiddlyDesktop-win64-v0.0.5
 mkdir -p output/linux32
+mkdir -p output/linux32/TiddlyDesktop-linux32-v0.0.5
 mkdir -p output/linux64
+mkdir -p output/linux64/TiddlyDesktop-linux64-v0.0.5
 
 # OS X 64-bit App
-cp -R nwjs/nwjs-v0.12.3-osx-x64/nwjs.app output/mac64/TiddlyWiki.app
-cp -R source output/mac64/TiddlyWiki.app/Contents/Resources/app.nw
-cp icons/app.icns output/mac64/TiddlyWiki.app/Contents/Resources/nw.icns
-cp Info.plist output/mac64/TiddlyWiki.app/Contents/Info.plist
+cp -R nwjs/nwjs-v0.12.3-osx-x64/nwjs.app output/mac64/TiddlyDesktop-mac64-v0.0.5/TiddlyWiki.app
+cp -R source output/mac64/TiddlyDesktop-mac64-v0.0.5/TiddlyWiki.app/Contents/Resources/app.nw
+cp icons/app.icns output/mac64/TiddlyDesktop-mac64-v0.0.5/TiddlyWiki.app/Contents/Resources/nw.icns
+cp Info.plist output/mac64/TiddlyDesktop-mac64-v0.0.5/TiddlyWiki.app/Contents/Info.plist
 
 # OS X 32-bit App
-cp -R nwjs/nwjs-v0.12.3-osx-ia32/nwjs.app output/mac32/TiddlyWiki.app
-cp -R source output/mac32/TiddlyWiki.app/Contents/Resources/app.nw
-cp icons/app.icns output/mac32/TiddlyWiki.app/Contents/Resources/nw.icns
-cp Info.plist output/mac32/TiddlyWiki.app/Contents/Info.plist
+# cp -R nwjs/nwjs-v0.12.3-osx-ia32/nwjs.app output/mac32/TiddlyDesktop-mac32-v0.0.5/TiddlyWiki.app
+# cp -R source output/mac32/TiddlyDesktop-mac32-v0.0.5/TiddlyWiki.app/Contents/Resources/app.nw
+# cp icons/app.icns output/mac32/TiddlyDesktop-mac32-v0.0.5/TiddlyWiki.app/Contents/Resources/nw.icns
+# cp Info.plist output/mac32/TiddlyDesktop-mac32-v0.0.5/TiddlyWiki.app/Contents/Info.plist
 
-# Windows 64-bit App
-cp -R nwjs/nwjs-v0.12.3-win-x64/* output/win64
-cp -R source/* output/win64
+# # Windows 64-bit App
+# cp -R nwjs/nwjs-v0.12.3-win-x64/* output/win64/TiddlyDesktop-win64-v0.0.5
+# cp -R source/* output/win64/TiddlyDesktop-win64-v0.0.5
 
-# Windows 32-bit App
-cp -R nwjs/nwjs-v0.12.3-win-ia32/* output/win32
-cp -R source/* output/win32
+# # Windows 32-bit App
+# cp -R nwjs/nwjs-v0.12.3-win-ia32/* output/win32/TiddlyDesktop-win32-v0.0.5
+# cp -R source/* output/win32/TiddlyDesktop-win32-v0.0.5
 
-# Linux 64-bit App
-cp -R nwjs/nwjs-v0.12.3-linux-x64/* output/linux64
-cp -R source/* output/linux64
+# # Linux 64-bit App
+# cp -R nwjs/nwjs-v0.12.3-linux-x64/* output/linux64/TiddlyDesktop-linux64-v0.0.5
+# cp -R source/* output/linux64/TiddlyDesktop-linux64-v0.0.5
 
-# Linux 32-bit App
-cp -R nwjs/nwjs-v0.12.3-linux-ia32/* output/linux32
-cp -R source/* output/linux32
+# # Linux 32-bit App
+# cp -R nwjs/nwjs-v0.12.3-linux-ia32/* output/linux32/TiddlyDesktop-linux32-v0.0.5
+# cp -R source/* output/linux32/TiddlyDesktop-linux32-v0.0.5
--- run.sh
+++ run.sh
@@ -11,5 +11,5 @@ popd
 
 ./bld.sh
 
-./output/mac64/TiddlyWiki.app/Contents/MacOS/nwjs
+./output/mac64/TiddlyDesktop-mac64-v0.0.5/TiddlyWiki.app/Contents/MacOS/nwjs
 
