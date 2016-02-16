c10487f5d646ce2a640755bd7d1e0ba554be838b
Matt Lauber <github@mklauber.com>
2016-02-16 11:51:33 -0500

    Cleaned up spaces vs tabs.

--- plugins/tiddlydesktop/modules/startup/handlers.js
+++ plugins/tiddlydesktop/modules/startup/handlers.js
@@ -52,21 +52,21 @@ exports.startup = function() {
 		return false;
 	});
 	$tw.rootWidget.addEventListener("tiddlydesktop-clone-wiki-path",function(event) {
-	    
-	    /* Find the main window */
-	    var window_url = $tw.desktop.windowList.decodeUrl("backstage://WikiListWindow")
-	    var main_window = $tw.desktop.windowList.find(window_url.WindowConstructor, window_url.info) 
-        
-        /* Ask for the new file name */
-        var src  = $tw.desktop.windowList.decodeUrl(event.param).info.pathname
-        var dest = main_window.window_nwjs.window.prompt("What should be the path to the new file?");
-        if(dest === null){ return false; } /* If the prompt returns null, the user cancelled the action, we should too. */
-        
-        fs.writeFileSync(dest,fs.readFileSync(src));
-        
-        $tw.desktop.windowList.openByUrl("file://"+dest);
-        
-        
+
+		/* Find the main window */
+		var window_url = $tw.desktop.windowList.decodeUrl("backstage://WikiListWindow")
+		var main_window = $tw.desktop.windowList.find(window_url.WindowConstructor, window_url.info) 
+
+		/* Ask for the new file name */
+		var src  = $tw.desktop.windowList.decodeUrl(event.param).info.pathname
+		var dest = main_window.window_nwjs.window.prompt("What should be the path to the new file?");
+		if(dest === null){ return false; } /* If the prompt returns null, the user cancelled the action, we should too. */
+
+		/* Copy the file */
+		fs.writeFileSync(dest,fs.readFileSync(src));
+
+		/* Add the new file */
+		$tw.desktop.windowList.openByUrl("file://"+dest);
 	});
 };
 
