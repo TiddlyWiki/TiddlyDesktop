bf867f79b6136c5f00beb93b59ef267802fa9b33
Jermolene <jeremy@jermolene.com>
2018-02-07 12:23:41 +0000

    Update to nwjs v0.28.1

--- download-nwjs.sh
+++ download-nwjs.sh
@@ -2,8 +2,8 @@
 
 # Download nw.js
 
-curl -o 'nwjs/nwjs-sdk-v0.28.0-win-x64.zip' 'https://dl.nwjs.io/v0.28.0/nwjs-sdk-v0.28.0-win-x64.zip'
-curl -o 'nwjs/nwjs-sdk-v0.28.0-win-ia32.zip' 'https://dl.nwjs.io/v0.28.0/nwjs-sdk-v0.28.0-win-ia32.zip'
-curl -o 'nwjs/nwjs-sdk-v0.28.0-linux-x64.tar.gz' 'https://dl.nwjs.io/v0.28.0/nwjs-sdk-v0.28.0-linux-x64.tar.gz'
-curl -o 'nwjs/nwjs-sdk-v0.28.0-linux-ia32.tar.gz' 'https://dl.nwjs.io/v0.28.0/nwjs-sdk-v0.28.0-linux-ia32.tar.gz'
-curl -o 'nwjs/nwjs-sdk-v0.28.0-osx-x64.zip' 'https://dl.nwjs.io/v0.28.0/nwjs-sdk-v0.28.0-osx-x64.zip'
+curl -o 'nwjs/nwjs-sdk-v0.28.1-win-x64.zip' 'https://dl.nwjs.io/v0.28.1/nwjs-sdk-v0.28.1-win-x64.zip'
+curl -o 'nwjs/nwjs-sdk-v0.28.1-win-ia32.zip' 'https://dl.nwjs.io/v0.28.1/nwjs-sdk-v0.28.1-win-ia32.zip'
+curl -o 'nwjs/nwjs-sdk-v0.28.1-linux-x64.tar.gz' 'https://dl.nwjs.io/v0.28.1/nwjs-sdk-v0.28.1-linux-x64.tar.gz'
+curl -o 'nwjs/nwjs-sdk-v0.28.1-linux-ia32.tar.gz' 'https://dl.nwjs.io/v0.28.1/nwjs-sdk-v0.28.1-linux-ia32.tar.gz'
+curl -o 'nwjs/nwjs-sdk-v0.28.1-osx-x64.zip' 'https://dl.nwjs.io/v0.28.1/nwjs-sdk-v0.28.1-osx-x64.zip'
