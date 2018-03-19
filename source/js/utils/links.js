3b617ffd50fee1fd4e19f4fcbddce76dafd26dd5
Jermolene <jeremy@jermolene.com>
2018-03-19 17:51:15 +0000

    Fix crash with SVG links

--- source/js/utils/links.js
+++ source/js/utils/links.js
@@ -13,11 +13,22 @@ exports.trapLinks = function(doc) {
 		// Check that we're not in an internal link
 		// "tc-tiddlylink" is for TW5, "tiddlyLink" for TWC
 		var link = $tw.desktop.utils.dom.findParentWithTag(event.target,"a");
-		if(link && link.href.slice(0,11) !== "javascript:" && link.href.slice(0,5) !== "blob:" && link.href.split("#")[0] !== window.location.href.split("#")[0]) {
-			$tw.desktop.gui.Shell.openExternal(link.href);
-			event.preventDefault();
-			event.stopPropagation();
-			return false;
+		if(link) {
+			var href;
+			if(link.namespaceURI === "http://www.w3.org/2000/svg") {
+				// SVG
+				href = link.href.baseVal.split("#");
+				href = (href[0] || doc.location.href.split("#")[0]) + "#" + href[1];
+			} else {
+				// HTML
+				href = link.href;
+			}
+			if(href && href.slice(0,11) !== "javascript:" && href.slice(0,5) !== "blob:" && href.split("#")[0] !== doc.location.href.split("#")[0]) {
+				$tw.desktop.gui.Shell.openExternal(href);
+				event.preventDefault();
+				event.stopPropagation();
+				return false;
+			}
 		}
 		return true;
 	},false);
