097467382370ca6da1a2f50649b92858bddb92f9
Jermolene <jeremy@jermolene.com>
2018-06-08 16:56:43 +0100

    Update to nwjs v0.31.1

--- bld.sh
+++ bld.sh
@@ -39,7 +39,7 @@ mkdir -p output/linux64/TiddlyDesktop-linux64-v0.0.14
 
 # OS X 64-bit App
 
-cp -R nwjs/nwjs-sdk-v0.28.1-osx-x64/nwjs.app output/mac64/TiddlyDesktop-mac64-v0.0.14/TiddlyDesktop.app
+cp -R nwjs/nwjs-sdk-v0.31.1-osx-x64/nwjs.app output/mac64/TiddlyDesktop-mac64-v0.0.14/TiddlyDesktop.app
 cp -R source output/mac64/TiddlyDesktop-mac64-v0.0.14/TiddlyDesktop.app/Contents/Resources/app.nw
 cp icons/app.icns output/mac64/TiddlyDesktop-mac64-v0.0.14/TiddlyDesktop.app/Contents/Resources/nw.icns
 cp Info.plist output/mac64/TiddlyDesktop-mac64-v0.0.14/TiddlyDesktop.app/Contents/Info.plist
@@ -50,17 +50,17 @@ do
 done
 
 # Windows 64-bit App
-cp -R nwjs/nwjs-sdk-v0.28.1-win-x64/* output/win64/TiddlyDesktop-win64-v0.0.14
+cp -R nwjs/nwjs-sdk-v0.31.1-win-x64/* output/win64/TiddlyDesktop-win64-v0.0.14
 cp -R source/* output/win64/TiddlyDesktop-win64-v0.0.14
 
 # # Windows 32-bit App
-cp -R nwjs/nwjs-sdk-v0.28.1-win-ia32/* output/win32/TiddlyDesktop-win32-v0.0.14
+cp -R nwjs/nwjs-sdk-v0.31.1-win-ia32/* output/win32/TiddlyDesktop-win32-v0.0.14
 cp -R source/* output/win32/TiddlyDesktop-win32-v0.0.14
 
 # # Linux 64-bit App
-cp -R nwjs/nwjs-sdk-v0.28.1-linux-x64/* output/linux64/TiddlyDesktop-linux64-v0.0.14
+cp -R nwjs/nwjs-sdk-v0.31.1-linux-x64/* output/linux64/TiddlyDesktop-linux64-v0.0.14
 cp -R source/* output/linux64/TiddlyDesktop-linux64-v0.0.14
 
 # # Linux 32-bit App
-cp -R nwjs/nwjs-sdk-v0.28.1-linux-ia32/* output/linux32/TiddlyDesktop-linux32-v0.0.14
+cp -R nwjs/nwjs-sdk-v0.31.1-linux-ia32/* output/linux32/TiddlyDesktop-linux32-v0.0.14
 cp -R source/* output/linux32/TiddlyDesktop-linux32-v0.0.14
