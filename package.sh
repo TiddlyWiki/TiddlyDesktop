8c1a4804995d5c25bbf74b9497eb7970201489a1
jeremy@jermolene.com <jeremy@jermolene.com>
2020-04-27 21:19:16 +0100

    Fix package.sh

--- package.sh
+++ package.sh
@@ -4,19 +4,21 @@
 
 # run this after bld.sh
 
+VERSION=$(./bin/get-version-number)
+
 # Zip them up
 pushd ./output/win32
-zip -r ../tiddlydesktop-win32-v$(./bin/get-version-number).zip *
+zip -r "../tiddlydesktop-win32-v$VERSION.zip" *
 popd
 pushd ./output/win64
-zip -r ../tiddlydesktop-win64-v$(./bin/get-version-number).zip *
+zip -r "../tiddlydesktop-win64-v$VERSION.zip" *
 popd
 pushd ./output/mac64
-zip --symlinks -r ../tiddlydesktop-mac64-v$(./bin/get-version-number).zip *
+zip --symlinks -r "../tiddlydesktop-mac64-v$VERSION.zip" *
 popd
 pushd ./output/linux32
-zip -r ../tiddlydesktop-linux32-v$(./bin/get-version-number).zip *
+zip -r "../tiddlydesktop-linux32-v$VERSION.zip" *
 popd
 pushd ./output/linux64
-zip -r ../tiddlydesktop-linux64-v$(./bin/get-version-number).zip *
+zip -r "../tiddlydesktop-linux64-v$VERSION.zip" *
 popd
