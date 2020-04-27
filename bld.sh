e9f7ad56c27149e3b2a4c4690d3a1a35804a5adc
jeremy@jermolene.com <jeremy@jermolene.com>
2020-04-27 21:04:51 +0100

    Simplify scripts by reading version number from package.json

--- bld.sh
+++ bld.sh
@@ -25,42 +25,42 @@ node propagate-version.js
 # Create the output directories
 mkdir -p output
 mkdir -p output/mac64
-mkdir -p output/mac64/TiddlyDesktop-mac64-v0.0.14
+mkdir -p output/mac64/TiddlyDesktop-mac64-v$(./bin/get-version-number)
 mkdir -p output/win32
-mkdir -p output/win32/TiddlyDesktop-win32-v0.0.14
+mkdir -p output/win32/TiddlyDesktop-win32-v$(./bin/get-version-number)
 mkdir -p output/win64
-mkdir -p output/win64/TiddlyDesktop-win64-v0.0.14
+mkdir -p output/win64/TiddlyDesktop-win64-v$(./bin/get-version-number)
 mkdir -p output/linux32
-mkdir -p output/linux32/TiddlyDesktop-linux32-v0.0.14
+mkdir -p output/linux32/TiddlyDesktop-linux32-v$(./bin/get-version-number)
 mkdir -p output/linux64
-mkdir -p output/linux64/TiddlyDesktop-linux64-v0.0.14
+mkdir -p output/linux64/TiddlyDesktop-linux64-v$(./bin/get-version-number)
 
 # For each platform, copy the stock nw.js binaries overlaying the "source" directory (and icons and plist for the Mac)
 
 # OS X 64-bit App
 
-cp -RH nwjs/nwjs-sdk-v0.45.2-osx-x64/nwjs.app output/mac64/TiddlyDesktop-mac64-v0.0.14/TiddlyDesktop.app
-cp -RH source output/mac64/TiddlyDesktop-mac64-v0.0.14/TiddlyDesktop.app/Contents/Resources/app.nw
-cp icons/app.icns output/mac64/TiddlyDesktop-mac64-v0.0.14/TiddlyDesktop.app/Contents/Resources/nw.icns
-cp Info.plist output/mac64/TiddlyDesktop-mac64-v0.0.14/TiddlyDesktop.app/Contents/Info.plist
+cp -RH nwjs/nwjs-sdk-v0.45.2-osx-x64/nwjs.app output/mac64/TiddlyDesktop-mac64-v$(./bin/get-version-number)/TiddlyDesktop.app
+cp -RH source output/mac64/TiddlyDesktop-mac64-v$(./bin/get-version-number)/TiddlyDesktop.app/Contents/Resources/app.nw
+cp icons/app.icns output/mac64/TiddlyDesktop-mac64-v$(./bin/get-version-number)/TiddlyDesktop.app/Contents/Resources/nw.icns
+cp Info.plist output/mac64/TiddlyDesktop-mac64-v$(./bin/get-version-number)/TiddlyDesktop.app/Contents/Info.plist
 
-for f in output/mac64/TiddlyDesktop-mac64-v0.0.14/TiddlyDesktop.app/Contents/Resources/*.lproj
+for f in output/mac64/TiddlyDesktop-mac64-v$(./bin/get-version-number)/TiddlyDesktop.app/Contents/Resources/*.lproj
 do
 	cp "./strings/InfoPlist.strings" "$f/InfoPlist.strings"
 done
 
 # Windows 64-bit App
-cp -RH nwjs/nwjs-sdk-v0.45.2-win-x64/* output/win64/TiddlyDesktop-win64-v0.0.14
-cp -RH source/* output/win64/TiddlyDesktop-win64-v0.0.14
+cp -RH nwjs/nwjs-sdk-v0.45.2-win-x64/* output/win64/TiddlyDesktop-win64-v$(./bin/get-version-number)
+cp -RH source/* output/win64/TiddlyDesktop-win64-v$(./bin/get-version-number)
 
 # # Windows 32-bit App
-cp -RH nwjs/nwjs-sdk-v0.45.2-win-ia32/* output/win32/TiddlyDesktop-win32-v0.0.14
-cp -RH source/* output/win32/TiddlyDesktop-win32-v0.0.14
+cp -RH nwjs/nwjs-sdk-v0.45.2-win-ia32/* output/win32/TiddlyDesktop-win32-v$(./bin/get-version-number)
+cp -RH source/* output/win32/TiddlyDesktop-win32-v$(./bin/get-version-number)
 
 # # Linux 64-bit App
-cp -RH nwjs/nwjs-sdk-v0.45.2-linux-x64/* output/linux64/TiddlyDesktop-linux64-v0.0.14
-cp -RH source/* output/linux64/TiddlyDesktop-linux64-v0.0.14
+cp -RH nwjs/nwjs-sdk-v0.45.2-linux-x64/* output/linux64/TiddlyDesktop-linux64-v$(./bin/get-version-number)
+cp -RH source/* output/linux64/TiddlyDesktop-linux64-v$(./bin/get-version-number)
 
 # # Linux 32-bit App
-cp -RH nwjs/nwjs-sdk-v0.45.2-linux-ia32/* output/linux32/TiddlyDesktop-linux32-v0.0.14
-cp -RH source/* output/linux32/TiddlyDesktop-linux32-v0.0.14
+cp -RH nwjs/nwjs-sdk-v0.45.2-linux-ia32/* output/linux32/TiddlyDesktop-linux32-v$(./bin/get-version-number)
+cp -RH source/* output/linux32/TiddlyDesktop-linux32-v$(./bin/get-version-number)
--- run.sh
+++ run.sh
@@ -3,12 +3,6 @@
 # build TiddlyWiki and TiddlyDesktop
 
 
-pushd ../TiddlyWiki5
-
-./bin/quick-bld.sh || exit 1
-
-popd
-
 ./bld.sh || exit 1
 
-./output/mac64/TiddlyDesktop-mac64-v0.0.14/TiddlyDesktop.app/Contents/MacOS/nwjs || exit 1
+./output/mac64/TiddlyDesktop-mac64-v$(./bin/get-version-number)/TiddlyDesktop.app/Contents/MacOS/nwjs --debug || exit 1
