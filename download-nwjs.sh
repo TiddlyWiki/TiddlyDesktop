3f33d596b31d3b70124c2ccd13ef4fbc780e9a9e
jeremy@jermolene.com <jeremy@jermolene.com>
2020-05-05 16:47:09 +0100

    Update to nwjs v0.45.5

--- download-nwjs.sh
+++ download-nwjs.sh
@@ -6,11 +6,11 @@
 
 # Download nw.js
 
-curl -o 'nwjs/nwjs-sdk-v0.45.4-win-x64.zip' 'https://dl.nwjs.io/v0.45.4/nwjs-sdk-v0.45.4-win-x64.zip' || exit 1
-curl -o 'nwjs/nwjs-sdk-v0.45.4-win-ia32.zip' 'https://dl.nwjs.io/v0.45.4/nwjs-sdk-v0.45.4-win-ia32.zip' || exit 1
-curl -o 'nwjs/nwjs-sdk-v0.45.4-linux-x64.tar.gz' 'https://dl.nwjs.io/v0.45.4/nwjs-sdk-v0.45.4-linux-x64.tar.gz' || exit 1
-curl -o 'nwjs/nwjs-sdk-v0.45.4-linux-ia32.tar.gz' 'https://dl.nwjs.io/v0.45.4/nwjs-sdk-v0.45.4-linux-ia32.tar.gz' || exit 1
-curl -o 'nwjs/nwjs-sdk-v0.45.4-osx-x64.zip' 'https://dl.nwjs.io/v0.45.4/nwjs-sdk-v0.45.4-osx-x64.zip' || exit 1
+curl -o 'nwjs/nwjs-sdk-v0.45.5-win-x64.zip' 'https://dl.nwjs.io/v0.45.5/nwjs-sdk-v0.45.5-win-x64.zip' || exit 1
+curl -o 'nwjs/nwjs-sdk-v0.45.5-win-ia32.zip' 'https://dl.nwjs.io/v0.45.5/nwjs-sdk-v0.45.5-win-ia32.zip' || exit 1
+curl -o 'nwjs/nwjs-sdk-v0.45.5-linux-x64.tar.gz' 'https://dl.nwjs.io/v0.45.5/nwjs-sdk-v0.45.5-linux-x64.tar.gz' || exit 1
+curl -o 'nwjs/nwjs-sdk-v0.45.5-linux-ia32.tar.gz' 'https://dl.nwjs.io/v0.45.5/nwjs-sdk-v0.45.5-linux-ia32.tar.gz' || exit 1
+curl -o 'nwjs/nwjs-sdk-v0.45.5-osx-x64.zip' 'https://dl.nwjs.io/v0.45.5/nwjs-sdk-v0.45.5-osx-x64.zip' || exit 1
 
 pushd nwjs
 
