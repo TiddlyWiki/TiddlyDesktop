72e5aa84b759dcf9e373bef6352cb551e9d8cff9
jeremy@jermolene.com <jeremy@jermolene.com>
2022-12-06 18:47:48 +0000

    Disable app "open" event
    Intended to fix #214
    
    See https://github.com/TiddlyWiki/TiddlyDesktop/issues/214#issuecomment-1339819665

--- source/js/main.js
+++ source/js/main.js
@@ -170,9 +170,10 @@ $tw.boot.boot(function() {
 		}
 	}
 	// Register for file open events
-	gui.App.on("open",function(p) {
-		$tw.desktop.windowList.openByPathname(p);
-	});
+	// Commented out as an attempt to fix https://github.com/TiddlyWiki/TiddlyDesktop/issues/214
+	// gui.App.on("open",function(cmdline) {
+	// 	$tw.desktop.windowList.openByPathname(p);
+	// });
 	// Open wiki list window if we haven't opened any other windows
 	if(!commandFlags.haveOpenedWindow) {
 		$tw.desktop.windowList.openByUrl("backstage://WikiListWindow",{mustQuitOnClose: true});		
