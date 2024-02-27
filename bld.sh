b69b195fedffe61c1b05f33a4ff52a1200034ea5
Robin Munn <rmunn@pobox.com>
2024-02-27 12:52:55 +0700

    Build in parallel so it takes one-sixth as long

--- bld.sh
+++ bld.sh
@@ -35,9 +35,20 @@ mkdir -p output/linux64/TiddlyDesktop-linux64-v$(./bin/get-version-number)
 
 # For each platform, copy the stock nw.js binaries overlaying the "source" directory (and icons and plist for the Mac)
 
-# OS X 64-bit App
+# Calculate nw.js version
 
-cp -RH nwjs/nwjs-sdk-v0.77.0-osx-x64/nwjs.app output/mac64/TiddlyDesktop-mac64-v$(./bin/get-version-number)/TiddlyDesktop.app
+if [ $# -gt 0 ]; then
+    NWJS_VERSION=$1
+elif [ -z "$NWJS_VERSION" ]; then
+    NWJS_VERSION=0.77.0
+fi
+
+# Build function definitions (which will be called at the end of the script)
+
+# OS X 64-bit App
+build_mac64() {
+
+cp -RH nwjs/nwjs-sdk-v${NWJS_VERSION}-osx-x64/nwjs.app output/mac64/TiddlyDesktop-mac64-v$(./bin/get-version-number)/TiddlyDesktop.app
 cp -RH source output/mac64/TiddlyDesktop-mac64-v$(./bin/get-version-number)/TiddlyDesktop.app/Contents/Resources/app.nw
 cp icons/app.icns output/mac64/TiddlyDesktop-mac64-v$(./bin/get-version-number)/TiddlyDesktop.app/Contents/Resources/nw.icns
 cp Info.plist output/mac64/TiddlyDesktop-mac64-v$(./bin/get-version-number)/TiddlyDesktop.app/Contents/Info.plist
@@ -47,9 +58,12 @@ do
 	cp "./strings/InfoPlist.strings" "$f/InfoPlist.strings"
 done
 
-# OS X Apple Silicon App
+}
 
-cp -RH nwjs/nwjs-sdk-v0.77.0-osx-arm64/nwjs.app output/macapplesilicon/TiddlyDesktop-macapplesilicon-v$(./bin/get-version-number)/TiddlyDesktop.app
+# OS X Apple Silicon App
+build_macapplesilicon() {
+
+cp -RH nwjs/nwjs-sdk-v${NWJS_VERSION}-osx-arm64/nwjs.app output/macapplesilicon/TiddlyDesktop-macapplesilicon-v$(./bin/get-version-number)/TiddlyDesktop.app
 cp -RH source output/macapplesilicon/TiddlyDesktop-macapplesilicon-v$(./bin/get-version-number)/TiddlyDesktop.app/Contents/Resources/app.nw
 cp icons/app.icns output/macapplesilicon/TiddlyDesktop-macapplesilicon-v$(./bin/get-version-number)/TiddlyDesktop.app/Contents/Resources/nw.icns
 cp Info.plist output/macapplesilicon/TiddlyDesktop-macapplesilicon-v$(./bin/get-version-number)/TiddlyDesktop.app/Contents/Info.plist
@@ -61,18 +75,60 @@ done
 
 xattr -c output/macapplesilicon/TiddlyDesktop-macapplesilicon-v$(./bin/get-version-number)/TiddlyDesktop.app
 
+}
+
 # Windows 64-bit App
-cp -RH nwjs/nwjs-sdk-v0.77.0-win-x64/* output/win64/TiddlyDesktop-win64-v$(./bin/get-version-number)
+build_win64() {
+cp -RH nwjs/nwjs-sdk-v${NWJS_VERSION}-win-x64/* output/win64/TiddlyDesktop-win64-v$(./bin/get-version-number)
 cp -RH source/* output/win64/TiddlyDesktop-win64-v$(./bin/get-version-number)
+}
 
 # # Windows 32-bit App
-cp -RH nwjs/nwjs-sdk-v0.77.0-win-ia32/* output/win32/TiddlyDesktop-win32-v$(./bin/get-version-number)
+build_win32() {
+cp -RH nwjs/nwjs-sdk-v${NWJS_VERSION}-win-ia32/* output/win32/TiddlyDesktop-win32-v$(./bin/get-version-number)
 cp -RH source/* output/win32/TiddlyDesktop-win32-v$(./bin/get-version-number)
+}
 
 # # Linux 64-bit App
-cp -RH nwjs/nwjs-sdk-v0.77.0-linux-x64/* output/linux64/TiddlyDesktop-linux64-v$(./bin/get-version-number)
+build_linux64() {
+cp -RH nwjs/nwjs-sdk-v${NWJS_VERSION}-linux-x64/* output/linux64/TiddlyDesktop-linux64-v$(./bin/get-version-number)
 cp -RH source/* output/linux64/TiddlyDesktop-linux64-v$(./bin/get-version-number)
+}
 
 # # Linux 32-bit App
-cp -RH nwjs/nwjs-sdk-v0.77.0-linux-ia32/* output/linux32/TiddlyDesktop-linux32-v$(./bin/get-version-number)
+build_linux32() {
+cp -RH nwjs/nwjs-sdk-v${NWJS_VERSION}-linux-ia32/* output/linux32/TiddlyDesktop-linux32-v$(./bin/get-version-number)
 cp -RH source/* output/linux32/TiddlyDesktop-linux32-v$(./bin/get-version-number)
+}
+
+if [ "$CI" = "true" ]; then
+    # Running in GitHub Actions, where each platform builds as a separate step, in parallel, with PLATFORM and ARCH variables supplied by the GitHub Actions script
+	case "$PLATFORM-$ARCH" in
+		osx-x64)
+			build_mac64
+			;;
+		osx-arm64)
+			build_macapplesilicon
+			;;
+		win-ia32)
+			build_win32
+			;;
+		win-x64)
+			build_win64
+			;;
+		linux-ia32)
+			build_linux32
+			;;
+		linux-x64)
+			build_linux64
+			;;
+	esac
+else
+    # Running at the command line, where each platfom builds one at a time in sequence
+	build_mac64
+	build_macapplesilicon
+	build_win32
+	build_win64
+	build_linux32
+	build_linux64
+fi
--- package.sh
+++ package.sh
@@ -7,22 +7,72 @@
 VERSION=$(./bin/get-version-number)
 
 # Zip them up
+package_win32() {
 pushd ./output/win32
 zip -r "../tiddlydesktop-win32-v$VERSION.zip" *
 popd
+}
+
+package_win64() {
 pushd ./output/win64
 zip -r "../tiddlydesktop-win64-v$VERSION.zip" *
 popd
+}
+
+package_mac64() {
 pushd ./output/mac64
 zip --symlinks -r "../tiddlydesktop-mac64-v$VERSION.zip" *
 popd
+}
+
+package_macapplesilicon() {
 pushd ./output/macapplesilicon
-sudo xattr -rc "./TiddlyDesktop-macapplesilicon-v$VERSION/TiddlyDesktop.app" && sudo codesign --force --deep --sign - "./TiddlyDesktop-macapplesilicon-v$VERSION/TiddlyDesktop.app"
+sudo xattr -rc "./TiddlyDesktop-macapplesilicon-v$VERSION/TiddlyDesktop.app" -a sudo codesign --force --deep --sign - "./TiddlyDesktop-macapplesilicon-v$VERSION/TiddlyDesktop.app"
 zip --symlinks -r "../tiddlydesktop-macapplesilicon-v$VERSION.zip" *
 popd
+}
+
+package_linux32() {
 pushd ./output/linux32
 zip -r "../tiddlydesktop-linux32-v$VERSION.zip" *
 popd
+}
+
+package_linux64() {
 pushd ./output/linux64
 zip -r "../tiddlydesktop-linux64-v$VERSION.zip" *
 popd
+}
+
+
+if [ "$CI" = "true" ]; then
+    # Running in GitHub Actions, where each platform builds as a separate step, in parallel, with PLATFORM and ARCH variables supplied by the GitHub Actions script
+	case "$PLATFORM-$ARCH" in
+		osx-x64)
+			package_mac64
+			;;
+		osx-arm64)
+			package_macapplesilicon
+			;;
+		win-ia32)
+			package_win32
+			;;
+		win-x64)
+			package_win64
+			;;
+		linux-ia32)
+			package_linux32
+			;;
+		linux-x64)
+			package_linux64
+			;;
+	esac
+else
+    # Running at the command line, where each platfom builds one at a time in sequence
+	package_mac64
+	package_macapplesilicon
+	package_win32
+	package_win64
+	package_linux32
+	package_linux64
+fi
