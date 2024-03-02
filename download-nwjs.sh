a7d492e7cc210800751901c83e5dd1bab29257e6
Robin Munn <rmunn@pobox.com>
2024-03-02 21:01:54 +0700

    Add GitHub Actions workflow to build TiddlyDesktop (#289)
    * Add GitHub Actions workflow to build TiddlyDesktop
    
    * Build in parallel so it takes one-sixth as long
    
    * Add release workflow
    
    Creates draft releases so they can be edited (adding release notes, for
    example) before they become visible to the public.

--- download-nwjs.sh
+++ download-nwjs.sh
@@ -4,18 +4,39 @@
 
 [[ -d ./nwjs ]] || mkdir ./nwjs || exit 1
 
+# Set default nw.js version if not specified in environment variable or parameter
+
+if [ $# -gt 0 ]; then
+    NWJS_VERSION=$1
+elif [ -z "$NWJS_VERSION" ]; then
+    NWJS_VERSION=0.77.0
+fi
+
 # Download nw.js
 
-curl --output 'nwjs/nwjs-sdk-v0.77.0-win-x64.zip' 'https://dl.nwjs.io/v0.77.0/nwjs-sdk-v0.77.0-win-x64.zip' || exit 1
-curl --output 'nwjs/nwjs-sdk-v0.77.0-win-ia32.zip' 'https://dl.nwjs.io/v0.77.0/nwjs-sdk-v0.77.0-win-ia32.zip' || exit 1
-curl --output 'nwjs/nwjs-sdk-v0.77.0-linux-x64.tar.gz' 'https://dl.nwjs.io/v0.77.0/nwjs-sdk-v0.77.0-linux-x64.tar.gz' || exit 1
-curl --output 'nwjs/nwjs-sdk-v0.77.0-linux-ia32.tar.gz' 'https://dl.nwjs.io/v0.77.0/nwjs-sdk-v0.77.0-linux-ia32.tar.gz' || exit 1
-curl --output 'nwjs/nwjs-sdk-v0.77.0-osx-x64.zip' 'https://dl.nwjs.io/v0.77.0/nwjs-sdk-v0.77.0-osx-x64.zip' || exit 1
-curl --output 'nwjs/nwjs-sdk-v0.77.0-osx-arm64.zip' 'https://dl.nwjs.io/v0.77.0/nwjs-sdk-v0.77.0-osx-arm64.zip' || exit 1
+if [ "$CI" = "true" ]; then
+    # Running in GitHub Actions, where each platform builds as a separate step, in parallel, with PLATFORM and ARCH and EXT variables supplied by the GitHub Actions script
+    curl --output "nwjs/nwjs-sdk-v${NWJS_VERSION}-${PLATFORM}-${ARCH}.${EXT}" "https://dl.nwjs.io/v${NWJS_VERSION}/nwjs-sdk-v${NWJS_VERSION}-${PLATFORM}-${ARCH}.${EXT}" || exit 1
+else
+    # Running at the command line, where each platfom builds one at a time in sequence
+    curl --output "nwjs/nwjs-sdk-v${NWJS_VERSION}-win-x64.zip" "https://dl.nwjs.io/v${NWJS_VERSION}/nwjs-sdk-v${NWJS_VERSION}-win-x64.zip" || exit 1
+    curl --output "nwjs/nwjs-sdk-v${NWJS_VERSION}-win-ia32.zip" "https://dl.nwjs.io/v${NWJS_VERSION}/nwjs-sdk-v${NWJS_VERSION}-win-ia32.zip" || exit 1
+    curl --output "nwjs/nwjs-sdk-v${NWJS_VERSION}-linux-x64.tar.gz" "https://dl.nwjs.io/v${NWJS_VERSION}/nwjs-sdk-v${NWJS_VERSION}-linux-x64.tar.gz" || exit 1
+    curl --output "nwjs/nwjs-sdk-v${NWJS_VERSION}-linux-ia32.tar.gz" "https://dl.nwjs.io/v${NWJS_VERSION}/nwjs-sdk-v${NWJS_VERSION}-linux-ia32.tar.gz" || exit 1
+    curl --output "nwjs/nwjs-sdk-v${NWJS_VERSION}-osx-x64.zip" "https://dl.nwjs.io/v${NWJS_VERSION}/nwjs-sdk-v${NWJS_VERSION}-osx-x64.zip" || exit 1
+    curl --output "nwjs/nwjs-sdk-v${NWJS_VERSION}-osx-arm64.zip" "https://dl.nwjs.io/v${NWJS_VERSION}/nwjs-sdk-v${NWJS_VERSION}-osx-arm64.zip" || exit 1
+fi
 
 pushd nwjs
 
+if [ ".$EXT" = ".tar.gz" ]; then
+ls *.gz | xargs -n 1 tar -xvzf || exit 1
+elif [ ".$EXT" = ".zip" ]; then
+ls *.zip | xargs -n 1 unzip || exit 1
+else
+# Running at command line, not in GitHub Actions
 ls *.gz | xargs -n 1 tar -xvzf || exit 1
 ls *.zip | xargs -n 1 unzip || exit 1
+fi
 
 popd
